be297d58294a46ecc69f205412442f20e5b62da2
Neymik <Neymik@users.noreply.github.com>
2021-07-07 14:52:51 +0300

    base func

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AnimeScrapper } from './animeScrapper';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly animeScrapper: AnimeScrapper
+    
+    ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('ongoing')
+  getOngoing() {
+    return this.animeScrapper.getOngoings();
+  }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+let animeScrapper = require('./animeScrapper')
+
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(1337);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
+
